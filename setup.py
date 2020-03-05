48145f3e0202e4393317f9d1b7bc2bcf919f1f48
Michail <mdmurzinov@edu.hse.ru>
2020-03-05 20:48:52 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+TOKEN = '1100119328:AAFAFviU6Xvy3lqqlIIkNVOzJQwcQooew9I'  # insert your token here
+PROXY = 'https://telegg.ru/orig/bot'  # proxy to connect against telegram ban
