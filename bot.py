806375a2cb8983c5dec5032fca45706e5dbcbed1
Michail <mdmurzinov@edu.hse.ru>
2020-03-13 17:33:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 
 import logging
 
+from datetime import timedelta, datetime
 from setup import PROXY, TOKEN
 from telegram import Bot, Update
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater
@@ -23,6 +24,7 @@ def update_log(func):
                 "user" : argc[0].effective_user.first_name,
                 "function" : func.__name__,
                 "message" : argc[0].message.text,
+                "date": argc[0].message.date,
                 })
         return func(*argc, **kwargs)
     return new_func
@@ -39,7 +41,9 @@ def start(update: Update, context: CallbackContext):
 def chat_help(update: Update, context: CallbackContext):
     """Send a message when the command /help is issued."""
     tmp = ["Введи команду /start для начала.",
-           "Введите команду /history, чтобы увидеть последние 5 действий."]
+           "Введите команду /history, чтобы увидеть последние 5 действий.",
+           "Введите команду /time, чтобы увидеть время, пройденное с последнего вашего сообщения.",
+           "Введите команду /date, чтобы увидеть текущую дату и время."]
     update.message.reply_text('\n'.join(tmp))
 
 @update_log
@@ -52,26 +56,47 @@ def error(update: Update, context: CallbackContext):
     """Log Errors caused by Updates."""
     logger.warning(f'Update {update} caused error {context.error}')
 
+@update_log
+def elapsed_time(update: Updater, context: CallbackContext):
+    user = update.effective_user.first_name
+    period = timedelta(0)
+    if len(LOG_HISTORY) > 1:
+        for i in range(len(LOG_HISTORY) - 2, -1, -1):
+            if LOG_HISTORY[i]["user"] == user:
+                time_delta = timedelta(hours=3, minutes=0, seconds=0)
+                period = LOG_HISTORY[i]["date"] + time_delta
+                period = datetime.now() - period
+                print(str(i) , str(period))
+                break;
+    update.message.reply_text(f"Прошло {period.seconds // 3600} часов, {(period.seconds % 3600) // 60} минут, {(period.seconds % 3600) % 60} секунд с последнего вашего сообщения.")
+
+@update_log
+def date(update: Updater, context: CallbackContext):
+    now = datetime.now()
+    update.message.reply_text(f"Дата: {now.day}.{now.month}.{now.year}\nВремя: {now.hour}:{now.minute}")
+
+@update_log
 def history(update: Updater, context: CallbackContext):
     I_start, end = 0, 0
     with open("history.txt", 'a') as handle:
-        answer = []
-        if len(LOG_HISTORY) == 0:
+        if len(LOG_HISTORY) == 1 and LOG_HISTORY[0]["function"] == "history":
             update.message.reply_text("There are no recent actions")
             handle.write("There are no recent actions\n")
-        elif len(LOG_HISTORY) < 5:
-            end = len(LOG_HISTORY)
-            answer.append("Last actions are:")
         else:
-            I_start, end = len(LOG_HISTORY) - 5, len(LOG_HISTORY)
-            answer.append("Last five actions are:")
-        for i in range(I_start, end):
-            answer.append(f"Action {i + 1}:")
-            for key, value in LOG_HISTORY[i].items():
-                answer.append(key + " : " + value)
-            answer[len(answer) - 1] += '\n'
-        update.message.reply_text("\n".join(answer))
-        handle.write("\n".join(answer) + '\n')
+            answer = []
+            if len(LOG_HISTORY) < 5:
+                end = len(LOG_HISTORY)
+                answer.append("Last actions are:")
+            else:
+                I_start, end = len(LOG_HISTORY) - 5, len(LOG_HISTORY)
+                answer.append("Last five actions are:")
+            for i in range(I_start, end):
+                answer.append(f"Action {i + 1}:")
+                for key, value in LOG_HISTORY[i].items():
+                    answer.append(key + " : " + str(value))
+                answer[len(answer) - 1] += '\n'
+            update.message.reply_text("\n".join(answer))
+            handle.write("\n".join(answer) + '\n')
 
 def main():
     bot = Bot(
@@ -84,6 +109,8 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('help', chat_help))
     updater.dispatcher.add_handler(CommandHandler('history', history))
+    updater.dispatcher.add_handler(CommandHandler('time', elapsed_time))
+    updater.dispatcher.add_handler(CommandHandler('date', date))
 
     # on noncommand i.e message - echo the message on Telegram
     updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
