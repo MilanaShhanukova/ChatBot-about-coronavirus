e39d3e226d9285a1b72565671e929f277e0d53ca
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-04-18 18:47:45 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+TOKEN = '941792804:AAHRqZ5ANMO__FU_7CQrGlsF5qM7kC-QRhE'  # insert your token here
+PROXY = 'https://telegg.ru/orig/bot'  # proxy to connect against telegram ban
