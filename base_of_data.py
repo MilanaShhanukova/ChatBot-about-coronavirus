735df645183c43c57bb1f710c3988fc5a09a456b
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-05-02 20:22:43 +0300

    Create base_of_data.py

---
+++ base_of_data.py
@@ -0,0 +1,14 @@
+import pymongo
+
+class DataBase_for_bot:
+    def __init__(self):
+        client = pymongo.MongoClient("localhost", 27017)
+        db = client.mongo_bd
+        self.corona_collection = db.corona_data
+
+    def inserting(self, data):
+        self.corona_collection.insert_one(data)
+
+    def finding(self):
+        return self.corona_collection.find()
+
