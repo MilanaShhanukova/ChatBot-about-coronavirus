26b9dbbd796988a54f164f12cc3e9c4cf5e32039
Michail <mdmurzinov@edu.hse.ru>
2020-05-05 21:03:53 +0300

    UPDATE

--- graphic_draw.py
+++ graphic_draw.py
@@ -9,86 +9,73 @@ class Statistics:
 
     URLS = {"confirmed": "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
             "deaths": "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
-            "recovered": "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"}
+            "recovered": "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv",
+            "Active": ""}
 
     def __init__(self):
-        self.now = ""
-        self.date = ""
+        self.dates = list()  # массив с датами до сегодня
+        self.data_set = list()  # массив с координатами
+        self.now = 0
+        self.date = 0
 
     @staticmethod
     def download_data(r):
         with open("current_info.csv", 'w', encoding='utf-8') as now_data:
             now_data.writelines(r.text)
 
-    def get_all_dates(self) -> list:
+    # def data(self, shift: int, location: str, aspect: str):
+    #     self.now = datetime.datetime.today() - datetime.timedelta(days=1)
+    #     self.date = datetime.datetime.today() - datetime.timedelta(days=shift)
+    #
+    #     r = requests.get(self.URLS[aspect])
+
+    def get_all_dates(self):
         dat = " "
-        dates = list()
         while self.date < self.now:
             date_1 = self.date.strftime("%m/%d/%Y")
             date_1 = date_1.split('/')
             for i in range(len(date_1) - 1):
                 date_1[i] = date_1[i].lstrip('0')
                 dat = "/".join(date_1)
-            dates.append(dat)
+            self.dates.append(dat)
             self.date = self.date + datetime.timedelta(days=1)
-        return dates
 
-    @staticmethod
-    def get_current_info(target_countries: list, dates: list) -> dict:
-        info = dict()
-        for country in target_countries:
-            for date in dates:
-                if date not in info.keys():
-                    info[date] = []
-                    info[date].append(int(country[date[:-2]]))
-                else:
-                    info[date].append(int(country[date[:-2]]))
-        return info
-
-    def write_data_in_data_set(self, location, dates: list) -> list:
+    def write_data(self, location):
         with open("current_info.csv", encoding='utf-8') as now_data:
             reader = csv.DictReader(now_data)
-            data_set = list()
-            target_countries = [row for row in reader if row["Country/Region"] == location]
-
-            info = self.get_current_info(target_countries, dates)
-
+            info = {}
+            for row in reader:
+                if row["Country/Region"] == location:
+                    for d in self.dates:
+                        if d not in info.keys():
+                            info[d] = []
+                            info[d].append(int(row[d[0:-2]]))
+                        else:
+                            info[d].append(int(row[d[0:-2]]))
             for data, value in info.items():
-                data_set.append(sum(value))
-            return data_set
+                self.data_set.append(sum(value))
 
-    @staticmethod
-    def correct_dates(dates: list) -> list:
-        new_dates = list()
-        for date in dates:
-            # Если день от 1 до 9
-            if len(date[:date.find('/')]) == 1:
-                new_dates.append('0' + date[:-5])
-            else:
-                new_dates.append(date[:-5])
-        return new_dates
-
-    @staticmethod
-    def graphic_draw(aspect: str, dates: list, data_set: list) -> None:
-        y = data_set
+    def graphic_draw(self, aspect: str):
+        y = self.data_set
         # соответствующие значения оси Y
-        x = dates
+        x = self.dates
         fig, ax = plt.subplots()
         ax.plot(x, y, color='r', linewidth=3)
         ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
         ax.xaxis.set_minor_locator(ticker.MultipleLocator(10))
-        ax.yaxis.set_major_locator(ticker.MultipleLocator(abs(data_set[-1] - data_set[0]) // 10))
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(abs(self.data_set[-1] - self.data_set[0]) // 10))
         ax.yaxis.set_minor_locator(ticker.MultipleLocator(10))
         #  Добавляем линии основной сетки:
         ax.grid(which='major', color='k')
         #  Включаем видимость вспомогательных делений:
         ax.minorticks_on()
         #  Теперь можем отдельно задавать внешний вид
-        #  Вспомогательной сетки:
+        #  вспомогательной сетки:
         ax.grid(which='minor',
                 color='gray',
                 linestyle=':')
-        # Название оси х
+
+        # название оси х
         plt.xlabel('days', fontsize=15)
         # имя оси Y
         plt.ylabel(aspect, fontsize=15)
@@ -102,14 +89,8 @@ class Statistics:
     def create_graphic_information(self, shift: int, location: str, aspect: str):
         self.now = datetime.datetime.today() - datetime.timedelta(days=1)
         self.date = datetime.datetime.today() - datetime.timedelta(days=shift)
-
-        req = requests.get(self.URLS[aspect])
-
-        self.download_data(req)
-
-        dates = self.get_all_dates()
-
-        data_set = self.write_data_in_data_set(location, dates)
-
-        dates = self.correct_dates(dates)
-        self.graphic_draw(aspect, dates, data_set)
+        r = requests.get(self.URLS[aspect])
+        self.download_data(r)
+        self.get_all_dates()
+        self.write_data(location)
+        self.graphic_draw(aspect)
--- tests/test_graphic_draw.py
+++ tests/test_graphic_draw.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest import mock
 from unittest.mock import patch
 from graphic_draw import Statistics
 
@@ -16,27 +15,19 @@ class TestGraphics(unittest.TestCase):
 
     def test_get_info(self):
         result_info = self.draw_graphic.get_current_info(self.test_countries, self.test_dates)
-
         self.assertEqual(result_info, self.test_info)
 
     def test_write_data(self):
         with patch("graphic_draw.Statistics.get_current_info") as mock_current_info:
             mock_current_info.return_value = self.test_info
             info = self.draw_graphic.write_data_in_data_set("France", self.test_dates)
-
             self.assertEqual([4, 3], info)
 
     def test_good_dates(self):
         test_bad_dates = ["4/20/2020", "1/10/2000", "11/12/2002"]
         good_dates = self.draw_graphic.correct_dates(test_bad_dates)
-
         self.assertEqual(good_dates, ['04/20', '01/10', '11/12'])
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
