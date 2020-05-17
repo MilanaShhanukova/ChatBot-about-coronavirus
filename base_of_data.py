0f8cda639e9ce28461a142e2885e2043b4872257
Милана Шханукова <mmshkhanukova@edu.hse.ru>
2020-05-17 21:02:34 +0300

    Update base_of_data.py

--- base_of_data.py
+++ base_of_data.py
@@ -5,10 +5,19 @@ class DataBase_for_bot:
         client = pymongo.MongoClient("localhost", 27017)
         db = client.mongo_bd
         self.corona_collection = db.corona_data
+        self.video_collection = db.video_corona
 
     def inserting(self, data):
-        self.corona_collection.insert_one(data)
+        self.video_collection.insert_one(data)
 
     def finding(self):
-        return self.corona_collection.find()
+        return self.video_collection.find()
+
+    def replacing(self, one_dict, new_dict):
+        return self.video_collection.replace_one(one_dict, new_dict)
+
+    def dropping(self):
+        return self.video_collection.drop()
+
+
 
